fc34a67795375b51a1fe0745fcefb88f5c39b03b
fabio <fabioviniciuslds@hotmail.com>
2022-09-20 21:53:59 -0300

    Create Delete, Exeptions and audit data

--- src/main/java/com/devfabio/dscatalog/services/CategoryService.java
+++ src/main/java/com/devfabio/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.devfabio.dscatalog.services;
 import com.devfabio.dscatalog.dto.CategoryDTO;
 import com.devfabio.dscatalog.entities.Category;
 import com.devfabio.dscatalog.repositories.CategoryRepository;
+import com.devfabio.dscatalog.services.exceptions.DatabaseException;
 import com.devfabio.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,7 +49,19 @@ public class CategoryService {
             entity = repository.save(entity);
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
-            throw new ResourceNotFoundException("Id not found" + id);
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(("Id not found " + id));
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity Violation! permission denied...");
         }
     }
 }
---
+++ src/main/java/com/devfabio/dscatalog/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.devfabio.dscatalog.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    private static final long SerialVersionUID= 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
