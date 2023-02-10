df84d6db35d5d84b62ab0ee64a8ee075d178f9ba
fabiaov <fabioviniciuslds@hotmail.com>
2023-02-10 13:19:33 -0300

    ProductService fix

--- src/main/java/com/devfabio/dscatalog/services/ProductService.java
+++ src/main/java/com/devfabio/dscatalog/services/ProductService.java
@@ -39,6 +39,7 @@ public class ProductService {
         return new ProductDTO(entity, entity.getCategories());
     }
 
+    @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
         copyDTOToEntity(dto, entity);
